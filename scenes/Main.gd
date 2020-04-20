6c02c9c1433ddafc47db3702f4c1448be5c1003d
David Jungmalm <david.jungmalm@gmail.com>
2020-04-20 19:04:10 +0200

    Added start scene and connected it to exit in pause scene

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -2,15 +2,22 @@ extends Node2D
 
 
 const SURGERY1 = preload("res://scenes/surgeries/Surgery1.tscn")
+const START = preload("res://scenes/Start.tscn")
 
 var current_surgery = null
+var start_scene = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Temp code
+	start_scene = START.instance()
+	start_scene.get_node("StartButton").connect("pressed",self,"_on_Start_pressed")
+	add_child(start_scene)
+	
+	
+func start_first_surgery():
 	set_current_surgery(SURGERY1.instance())
 	
-
 func set_current_surgery(surgery_instance):
 	current_surgery = surgery_instance
 	add_child(current_surgery)
@@ -21,14 +28,22 @@ func _on_Pause_current_surgery():
 	$Pause.visible = true
 	# Display pause scene
 
+func _on_Start_pressed():
+	remove_child(start_scene)
+	start_first_surgery()
+
+func _on_Exit_pressed():
+	$Pause.visible = false
+	remove_child(current_surgery)
+	start_scene = START.instance()
+	start_scene.get_node("StartButton").connect("pressed",self,"_on_Start_pressed")
+	add_child(start_scene)
 
 func _on_Restart_pressed():
 	$Pause.visible = false
 	remove_child(current_surgery)
 	set_current_surgery(SURGERY1.instance())
 	
-
-
 func _on_Continue_pressed():
 	$Pause.visible = false
 	current_surgery.start()
